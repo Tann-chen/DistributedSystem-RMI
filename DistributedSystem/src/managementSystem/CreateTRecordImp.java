27dbddd2feab3a927e9cbe80059ba20e90ae9dbe
grey <GreyLee369@gmail.com>
2017-05-30 14:04:30 -0400

    "create TRecord Imp"

---
+++ DistributedSystem/src/managementSystem/CreateTRecordImp.java
@@ -0,0 +1,22 @@
+package managementSystem;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import interfaces.CreateTRecord;
+import records.TeacherRecord;
+
+public class CreateTRecordImp extends UnicastRemoteObject implements CreateTRecord{
+
+	protected CreateTRecordImp() throws RemoteException {
+		super();
+	}
+
+	@Override
+	public boolean createTRecord(String firstName, String lastName, String address, String phone, String specialization,
+			String location) throws RemoteException {
+		TeacherRecord teacherRecord = new TeacherRecord(firstName, lastName, address, phone, specialization, location);
+		
+		return false;
+	}
+
+}
