48ddde5cac941c79df940c518a923e7a94121f2d
grey <GreyLee369@gmail.com>
2017-05-30 13:53:51 -0400

    "Servers"

---
+++ DistributedSystem/src/managementSystem/DDO.java
@@ -0,0 +1,12 @@
+package managementSystem;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DDO {
+	public static void main(String[] args) {
+		HashMap<Character, ArrayList<Record>> hashMap = new HashMap<>();
+		File file = new File("DDO.txt");
+	}
+}
---
+++ DistributedSystem/src/managementSystem/Laval.java
@@ -0,0 +1,12 @@
+package managementSystem;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Laval {
+	public static void main(String[] args) {
+		HashMap<Character, ArrayList<Record>> hashMap = new HashMap<>();
+		File file = new File("Laval.txt");
+	}
+}
