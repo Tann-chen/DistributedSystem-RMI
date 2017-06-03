c50c1272eee45cccab2e8ceae6b166fd36529a47
Grey <GreyLee369@gmail.com>
2017-06-03 11:21:22 -0400

    "servers receive UPD"

--- DistributedSystem/src/servers/DDO.java
+++ DistributedSystem/src/servers/DDO.java
@@ -1,8 +1,11 @@
 package servers;
 
 import java.io.File;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import thread.MyThread;
 
 
 public class DDO {
@@ -10,11 +13,32 @@ public class DDO {
 
 	public static void main(String[] args) throws Exception {
 		File logFile = new File("ddo.txt");
-		centerServer= new CenterServerImp(logFile);
+		centerServer= new CenterServerImp(logFile,"DDO");
 
 		Registry registry = LocateRegistry.createRegistry(3002);
 		registry.bind("DDOCenter", centerServer);
 		
 		System.out.println("DDO");
+		
+		DatagramSocket datagramSocket = null;
+		
+		try {
+			datagramSocket = new DatagramSocket(6789);
+			byte[] buffer = new byte[1000];
+			
+			while(true){
+				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+				datagramSocket.receive(request);
+				new MyThread(request.getAddress(),request.getPort(),datagramSocket);
+			}
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			if(datagramSocket != null)
+				datagramSocket.close();
+		}
+		
+		
 	}
 }
