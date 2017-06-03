e71513430c6da2e1cf82a68a501994c91797ab9b
Grey <GreyLee369@gmail.com>
2017-06-03 11:20:33 -0400

    "create Mythread"

--- DistributedSystem/src/thread/MyThread.java
+++ DistributedSystem/src/thread/MyThread.java
@@ -1,11 +1,27 @@
 package thread;
 
-public class MyThread implements Runnable{
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
+public class MyThread extends Thread{
+	InetAddress address;
+	int port;
+	DatagramSocket datagramSocket;
+
+	public MyThread(InetAddress address, int port, DatagramSocket datagramSocket) {
+		this.address = address;
+		this.port = port;
+		this.datagramSocket = datagramSocket;
+	}
+
+	
 	@Override
 	public void run() {
 		
 		
+//		DatagramPacket reply = new DatagramPacket(buf, length, address, port);
+//		datagramSocket.send(reply);
 	}
 
 }
